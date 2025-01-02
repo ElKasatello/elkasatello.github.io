8b8353fbb09f97a8b8deb148707ab30effedf2c3
ElKasatello <103767348+ElKasatello@users.noreply.github.com>
2025-01-02 10:54:50 +0300

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -33,14 +33,14 @@ suite('Unit Tests', function () {
   suite('Equality', function () {
     // #5
     test('#equal, #notEqual', function () {
-      assert.equal(12, '12', 'Numbers are coerced into strings with ==');
+      assert.notEqual(12, '12', 'Numbers are coerced into strings with ==');
       assert.equal({ value: 1 }, { value: 1 }, '== compares object references');
-      assert.equal(6 * '2', '12');
+      assert.notEqual(6 * '2', '12');
       assert.notEqual(6 + '2', '12');
     });
     // #6
     test('#strictEqual, #notStrictEqual', function () {
-      assert.notStrictEqual(6, '6');
+      assert.strictEqual(6, '6');
       assert.strictEqual(6, 3 * 2);
       assert.notStrictEqual(6 * '2', 12);
       assert.strictEqual([1, 'a', {}], [1, 'a', {}]);
